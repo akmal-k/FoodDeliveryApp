ba53251387d294239f810429f15372bb1106a801
Akmal <akmalkuchkorov98@gmail.com>
2022-03-29 23:33:52 +0900

    order screen

--- constants/maps.js
+++ constants/maps.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY = "AIzaSyD_lMXvGFGsiO2qhvX7Vs49r-sdICRtrAQ"
+const GOOGLE_API_KEY = 'AIzaSyA6F51dbSh00Ok4zKu0rP6b3YaYwRSs4H0';
 
-export default GOOGLE_API_KEY
+export default GOOGLE_API_KEY;
